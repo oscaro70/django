627fc3ac6380fdfa8148eac718b75ff1d5adae1a
oscaro70 <oscar_o_a@hotmail.com>
2025-10-31 22:19:44 -0600

    first commit

--- backend/settings.py
+++ backend/settings.py
@@ -14,20 +14,21 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+import os
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9s0v(t-=u8bqirsbvki*qb31))_rqq@ffpkx&ep^r$*h6fumbd'
-
+SECRET_KEY = os.environ.get('SECRET_KEY', default='snsdsdiijhdhikkk')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
-
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 # Application definition
 
 INSTALLED_APPS = [
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -73,17 +75,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-import pymysql
-pymysql.install_as_MySQLdb()
+
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'crud_db',
-        'USER': 'root',
-        'PASSWORD': '',  # deja vacío solo si tu root no tiene contraseña
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'NAME': 'railway',                # tu base de datos
+        'USER': 'root',                   # usuario
+        'PASSWORD': 'yIJCYBFnAUSvihhjPulRNBwIIBYpcwaF',     # reemplaza con tu password
+        'HOST': 'shuttle.proxy.rlwy.net',# host de Railway
+        'PORT': '17398',                  # puerto personalizado
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
@@ -125,7 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
