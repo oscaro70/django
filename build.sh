047b3fd0051009136791458acbe8fde47d7b9b33
oscaro70 <oscar_o_a@hotmail.com>
2025-10-31 22:58:18 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
-set -o errexit
+set -o errexit  # Sale del script si hay un error
 
-pip install -r requirements.txt
+# Instalar las dependencias
+pip install --no-cache-dir -r requirements.txt
 
+# Recopilar los archivos estáticos de Django
 python manage.py collectstatic --noinput
 
-python manage.py migrate
+# Aplicar las migraciones a la base de datos
+python manage.py migrate --noinput
+
+# Ejecutar el servidor de Django o Gunicorn (si es necesario)
+# gunicorn backend.wsgi:application
